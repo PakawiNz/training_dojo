5e95ca6126fb3bde29f53f18e2f99b8f25359c36
behbeh <behbeh@mse.com>
2016-10-14 16:59:10 +0700

    finished EXM070

--- training_1/urls.py
+++ training_1/urls.py
@@ -13,4 +13,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', master_views.home),
     url(r'^forms/EXM010/(?P<method>.*)/$', 'media.forms.EXM010.frmEXM010.dispatcher'),
+    
+    url(r'^EXM070/(?P<method>.*)/$', 'media.forms.EXM070.frmEXM070.calculator'),
+    url(r'^EXM070/$', 'media.forms.EXM070.frmEXM070.load'),
 )
